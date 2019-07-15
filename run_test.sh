90ceb6ab2c7c51a5653c7f7d21be9708c3f1e137
yaoyiran <yaoyiran@umich.edu>
2019-07-15 15:57:31 +0800

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-rm /media/data/nmt3/checkpoints/checkpoint_1_0.pt
-
-for file in $(ls /media/data/nmt3/checkpoints);
+rm /workspace/checkpoints/checkpoint_1_0.pt
+rm /workspace/results
+mkdir /workspace/results
+for file in $(ls /workspace/checkpoints);
 do
 #    if [[ $file = *.pt ]]; then 
     echo "Test The Checkpoint: $file"
     python generate.py /workspace/data-bin/wmt14_en_de_joined_dict \
-      --path /media/data/nmt3/checkpoints/$file \
+      --path /workspace/checkpoints/$file \
       --beam 4 --remove-bpe > /workspace/results/test.$file.out
     tail -n 2 /workspace/results/test.$file.out
 #    fi
