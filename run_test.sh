b531ffac68dbbe3bac93185dc44e4fce01fd0aca
yaoyiran <yaoyiran@umich.edu>
2019-07-15 16:02:47 +0800

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm /workspace/checkpoints/checkpoint_1_0.pt
-# rm -r /workspace/results
+rm -f -r /workspace/results
 mkdir -p /workspace/results
 for file in $(ls /workspace/checkpoints);
 do
